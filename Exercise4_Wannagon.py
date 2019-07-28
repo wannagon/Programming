929632af7038aeec30a6cf8eb5e5cd1e53e38471
wannagon <52350395+wannagon@users.noreply.github.com>
2019-07-28 22:48:35 +0700

    Add files via upload

---
+++ Exercise4_Wannagon.py
@@ -0,0 +1,9 @@
+q=80
+e=87
+t=85
+v=82
+print("------Your Score------")
+print("Foundation English",":",q)
+print("General Business",":",e)
+print("Introduction to Computer",":",t)
+print("Computer Programming",":",v)
